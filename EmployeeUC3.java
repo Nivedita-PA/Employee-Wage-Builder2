651fc32fab20d455cc77fc81d3cccce2df9700c5
Nivedita-PA <npal4661@gmail.com>
2021-04-17 02:23:37 +0530

    [Nivedita]: added the part time wage

---
+++ EmployeeUC3.java
@@ -0,0 +1,11 @@
+public class EmployeeUC3 {
+
+	public static void main(String[] args) {
+		int wage_Per_Hr = 20;
+		int partime_Working_Hrs_Day = 8;
+		
+		int daily_Wage = wage_Per_Hr * partime_Working_Hrs_Day;
+		System.out.println("Daily part time wage for each employee");
+		System.out.println(daily_Wage);
+	}
+}
