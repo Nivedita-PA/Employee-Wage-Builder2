02a1d27f8312d1129a8003c62812253b40f6a23b
Nivedita-PA <npal4661@gmail.com>
2021-04-17 02:51:48 +0530

    [Nivedita]: added the monthly wage function

--- EmployeeUC5.java
+++ EmployeeUC5.java
@@ -1,27 +1,30 @@
 public class EmployeeUC5 {
 	
 	public static final int full_Time = 1;
-	public static final int part_Time = 2;
+	public static final int half_Time = 2;
 	public static final int emp_Rate_Per_Hr = 20;
+	public static final int num_Of_Working_Days = 20;
 
 	public static void main(String[] args) {
-        int month_Days = 20 ;
-		int empHrs = 0;
-		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		System.out.println("Employee Wage: " + empCheck);
+
+		int empHrs = 0, empWage = 0, totalWage = 0;
+		
+		for (int day = 0; day < num_Of_Working_Days; day++) {
+	double empCheck = Math.floor(Math.random() * 10) % 3;
 		switch ((int)empCheck) {
 		case full_Time:
 			empHrs = 8;
 			break;
-		case part_Time:
+		case half_Time:
 			empHrs = 4;
 			break;
 		default:
-			break; 
-		}
-		empWage = empHrs * emp_Rate_Per_Hr * month_Days;
+			empHrs = 0; 
+		}	
+		empWage = empHrs * emp_Rate_Per_Hr;
+		totalWage += empWage;
 		System.out.println("Employee Wage: " + empWage);
-	
+	}
+		System.out.println("Total employee Wage: " +totalWage);
 }
 }
